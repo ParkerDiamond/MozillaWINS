71034c4b36a2c1c14eee3bf4e2ba24e55d8c8b6a
jaredmichaelsmith <jared@jaredsmith.io>
2017-12-04 15:15:29 -0500

    Make C++

--- src/simulation/server.cpp
+++ src/simulation/server.cpp
@@ -4,7 +4,61 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <iostream>
+#include <cstdlib>
+#include <itpp/comm/turbo.h>
 #define PORT 8080
+
+using namespace std;
+using namespace itpp;
+
+class GenericTurbo
+{
+    public:
+	Turbo_Codec codec;
+	ivec generator, interleaver;
+	int constraint;
+
+	GenericTurbo()
+	{
+		generator = ivec(2);
+		generator(0) = 013;
+		generator(1) = 013;
+		constraint = 4;
+		interleaver = wcdma_turbo_interleaver_sequence(320);
+		codec.set_parameters(generator, generator, constraint, interleaver);
+	}
+};
+
+
+int main()
+{
+	GenericTurbo *codec;
+	bvec input, encoded, decoded;
+
+	codec = new GenericTurbo();
+	input = randb(972-12); 
+
+	for(int i=0;i<input.size();i++) cout << input(i);
+	cout << endl;
+
+	codec->codec.encode(input, encoded);
+	codec->codec.decode(to_vec(encoded), decoded);
+
+	cout << codec->codec.get_Ncoded() << endl;
+	
+	cout << endl;
+	for(int i=0;i<decoded.size();i++) 
+	{
+		decoded(i) ^= 1;
+		cout << decoded(i);
+	}
+	cout << endl;
+
+	return 0;
+}
+
+
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket, valread;
