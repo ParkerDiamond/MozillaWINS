b4006fc6d9e8016293e67ead46380e8d8b6d686e
Kyle Birkeland <kylebirkeland@gmail.com>
2017-12-09 18:43:02 -0500

    Added function to simulate corruption and to simulate transmissions with corruption

--- src/simulation/SimpleTurbo.cpp
+++ src/simulation/SimpleTurbo.cpp
@@ -1,34 +1,69 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <random>
 #include "Transcoder.h"
 
 using namespace std;
 using namespace itpp;
 
-int main()
+void corrupt(vec &trans, double perc_corruption)
+{
+    int ncorrupt = int(perc_corruption * trans.size());
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, trans.size() - 1);
+
+    for(int i=0; i < ncorrupt; i++) {
+        int idx = dis(gen);
+        trans(idx) = -1 * trans(idx);
+    }
+}
+
+// Encodes data, simulated bit errors, and decodes data in place
+void transmit(char *data, int len, double error)
 {
     Transcoder *coder;
     bvec in, out;
     vec trans;
-    
-    in = randb(321);
- 
+
+    for(int i = 0; i < len; i++) {
+        in = concat(in, dec2bin(8, data[i]));
+    }
+
+    coder = new Transcoder();
+    coder->encode(in, trans);
+    corrupt(trans, error);
+    coder->decode(trans, out);
+
+    for(int i = 0; i < len; i = i + 8) {
+        data[i] = bin2dec(out(i, i+7));
+    }
+
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    Transcoder *coder;
+    bvec in, out;
+    vec trans;
+    double perc_corruption = 0.15;
+    int corrupt_bits = 0;
+    in = randb(320);
+
     coder = new Transcoder();
 
     coder->encode(in, trans);
 
-    for(int i=0;i<trans.size();i+=32)
-    {
-        if(trans(i) == -1) trans(i) = 1;
-        else trans(i) = -1;
-    }
+    corrupt(trans, perc_corruption);
 
     coder->decode(trans, out);
 
+    for(int i=0;i<in.size();i++) if ((in(i) ^ out(i)) != 0) corrupt_bits++;
 
-    for(int i=0;i<in.size();i++) cout << (in(i) ^ out(i));
-    cout << endl;
+    cout << perc_corruption << ',' << corrupt_bits << ',' << ((double)corrupt_bits)/out.size() << endl;
 
     return 0;
 }
+
